bec3220aaaa29f1d52330c01cd13e9b241cecd47
prasaks <akkamma_jabin@dell.com>
2019-03-28 09:46:15 +0530

    movie-fun

---
+++ components/albums/src/main/java/org/superbiz/moviefun/albums/AlbumsBean.java
@@ -0,0 +1,42 @@
+package org.superbiz.moviefun.albums;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import org.superbiz.moviefun.albums.Album;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaQuery;
+import java.util.List;
+
+@Repository
+public class AlbumsBean {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Transactional
+    public void addAlbum(Album album) {
+        entityManager.persist(album);
+    }
+
+    public Album find(long id) {
+        return entityManager.find(Album.class, id);
+    }
+
+    public List<Album> getAlbums() {
+        CriteriaQuery<Album> cq = entityManager.getCriteriaBuilder().createQuery(Album.class);
+        cq.select(cq.from(Album.class));
+        return entityManager.createQuery(cq).getResultList();
+    }
+
+    @Transactional
+    public void deleteAlbum(Album album) {
+        entityManager.remove(album);
+    }
+
+    @Transactional
+    public void updateAlbum(Album album) {
+        entityManager.merge(album);
+    }
+}
